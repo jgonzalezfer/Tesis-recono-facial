c202f5307ea5f3cdb865b27547d9894e72e07b67
Joel Gonzalez Fernandez <joel.gonzalez5115@gmail.com>
2022-10-30 22:54:27 -0300

    primer commit frontend

---
+++ src/app/pages/deteccion/deteccion.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-deteccion',
+  templateUrl: './deteccion.component.html',
+  styleUrls: ['./deteccion.component.css']
+})
+export class DeteccionComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
